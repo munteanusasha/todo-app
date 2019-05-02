37e7a95f62fd1fea1a09a717f20aff802995c471
spark <spark@spark.spark.debian.me>
2019-05-02 15:21:07 +0300

    changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let todolist = [];
-let item = document.getElementsByClassName("updateTodo");
+let item = String(document.getElementsByClassName("updateTodo"));
+let countId = 0;
 
 function updateTodo(){
-    
+    todolist.push({id:countId, item: item});
+    document.getElementsByClassName("list").innerHTML="";
 }
 
 function searchInTodo(){
